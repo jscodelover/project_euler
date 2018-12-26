1522543a2bae31fbfd76f76a4b6247c523e84b6e
manisha1896 <manisha.4918@gmail.com>
2018-12-26 22:10:24 +0530

    first commit

---
+++ multiple_of_3_and_5/inex.js
@@ -0,0 +1,22 @@
+/**
+ *
+ * @param {number} limit - upto what natural number we want to find the multiple sum.
+ * @returns {number} sum - sum of all the multiples of 3 or 5.
+ *
+ */
+
+function multiple(limit) {
+  if (limit < 1000) {
+    let sum = 0;
+    for (let i = 1; i <= limit; i++) {
+      if (i % 3 === 0 || i % 5 === 0) {
+        sum += i;
+      }
+    }
+    return sum;
+  }
+  return 0;
+}
+
+const result = multiple(153);
+console.log(result);
