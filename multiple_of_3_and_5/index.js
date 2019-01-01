8c1d0a56863cc1d9b8799e7ccdf1295ea2eedb77
manisha1896 <manisha.4918@gmail.com>
2019-01-01 14:04:09 +0530

    jsdoc update

--- multiple_of_3_and_5/index.js
+++ multiple_of_3_and_5/index.js
@@ -1,22 +1,22 @@
+"use strict";
 /**
  *
+ * @Problem : If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+   Find the sum of all the multiples of 3 or 5 below 1000. 
  * @param {number} limit - upto what natural number we want to find the multiple sum.
  * @returns {number} sum - sum of all the multiples of 3 or 5.
  *
  */
 
-function multiple(limit) {
-  if (limit < 1000) {
-    let sum = 0;
-    for (let i = 1; i < limit; i++) {
-      if (i % 3 === 0 || i % 5 === 0) {
-        sum += i;
-      }
+function multiple() {
+  let sum = 0;
+  for (let i = 1; i < 1000; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
     }
-    return sum;
   }
-  return 0;
+  return sum;
 }
 
-const result = multiple(153);
+const result = multiple();
 console.log(result);
