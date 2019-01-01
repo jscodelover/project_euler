ae838efc715a81d360270a61c1e90c9a865c29a6
manisha1896 <manisha.4918@gmail.com>
2019-01-01 15:36:16 +0530

    largest prime factor

--- Largest_Prime_Factor/index.js
+++ Largest_Prime_Factor/index.js
@@ -5,9 +5,32 @@
  * @Problem :  The prime factors of 13195 are 5, 7, 13 and 29.
     What is the largest prime factor of the number 600851475143 ?
  * @returns {number} result - largest prime factor    
+ * @function largest_prime_factor - find th largest prime factor
+ * 
+ * @function multiple - find the first multiple of argument n. eg(first multiple of 16 is 2.)
+ * @param {number} n - for which multiple is to be found.
  */
 
-function largest_prime_factor() {}
+function multiple(n) {
+  for (let i = 2; i <= n / 2; i++) {
+    if (n % i === 0) {
+      return i;
+    }
+  }
+  return n;
+}
+
+function largest_prime_factor() {
+  let divisior = dividend;
+  let result;
+
+  while (divisior > 1) {
+    divisior = multiple(dividend);
+    dividend = dividend / divisior;
+    if (divisior !== 1) result = divisior;
+  }
+  return result;
+}
 
 const result = largest_prime_factor();
 console.log(result);
